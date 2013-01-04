e4c7efb0f12e362fb606ab28a1334540d967c473
superjom <superjom@gmail.com>
2013-01-04 10:58:28 +0800

    UserIDTranser ok
    userid 的转化工具
    测试通过

---
+++ redog/dataloader.h
@@ -0,0 +1,14 @@
+/*
+ * dataloader.h
+ *
+ *  Created on: Jan 3, 2013
+ *      Author: chunwei
+ */
+
+#ifndef DATALOADER_H_
+#define DATALOADER_H_
+
+
+
+
+#endif /* DATALOADER_H_ */
--- redog/hell.cpp
+++ redog/hell.cpp
@@ -5,10 +5,27 @@
  *      Author: chunwei
  */
 #include <iostream>
+#include <vector>
+#include "UserIDTransfer.h"
 using namespace std;
+using namespace redog;
+
+void testUserIDTransfer(){
+	UserIDTransfer it;
+    it.show(1);
+	vector<int> uids;
+	uids.push_back(685);
+	uids.push_back(6510);
+	for(int i=0; i<uids.size(); ++i){
+		int pos = it.f_to_a(uids[i]);
+		cout<<"find "<<uids[i]<<"  pos: "<<pos<<endl;
+	}
+}
+
 
 int main(){
 	cout<<"hello"<<endl;
+	testUserIDTransfer();
 	return 0;
 }
 
